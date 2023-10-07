b80bd6522eb5714e4eaea720127dccc6b27dda42
rmob <riley.obryan@gmail.com>
2023-10-07 11:58:08 -0500

    fixed loading message, added error handling to useFetch

--- tripago/src/components/TripList.js
+++ tripago/src/components/TripList.js
@@ -6,8 +6,8 @@ import './TripList.css'
 
 export default function TripList() {
     // const [trips, setTrips] = useState([])
-    const [url, setUrl] = useState('http://localhost:3000/trips')
-    const { data: trips } = useFetch(url)
+    const [url, setUrl] = useState('http://localhost:3000/kygkjhg')
+    const { data: trips, isPending, error } = useFetch(url)
 
     // const fetchTrips = useCallback (async () => {
     //   const response = await fetch(url)
@@ -31,6 +31,8 @@ export default function TripList() {
   return (
     <div className="trip-list">
       <h2>Trip List</h2>
+      {isPending && <div>Loading trips...</div>}
+      {error && <div> {error} </div>}
       <ul>
         {trips && trips.map(trip => (
           <li key={trip.id}>
